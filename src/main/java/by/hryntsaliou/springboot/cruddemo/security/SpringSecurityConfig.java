6dfc30576ae1b18b2b36174eab89e273dbf1e926
Nikita <hrynts2002@gmail.com>
2024-02-01 16:01:09 +0300

    Added URL's restriction based on roles

--- src/main/java/by/hryntsaliou/springboot/cruddemo/security/SpringSecurityConfig.java
+++ src/main/java/by/hryntsaliou/springboot/cruddemo/security/SpringSecurityConfig.java
@@ -2,12 +2,35 @@ package by.hryntsaliou.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
@@ -21,7 +44,7 @@ public class SpringSecurityConfig {
         UserDetails anton = User.builder()
                 .username("srulik")
                 .password("{noop}123")
-                .roles("EMPLOYEE", "MANGER")
+                .roles("EMPLOYEE", "MANAGER")
                 .build();
 
         UserDetails stas = User.builder()
